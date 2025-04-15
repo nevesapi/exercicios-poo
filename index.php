1426f6d23bb7087e2fec03d61ae5d6e6dc062753
Denis Neves <nevesdenis35@gmail.com>
2025-04-15 09:52:27 -0300

    Cria construct para livro

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 01</title>
-</head>
-<body>
-    <h1>Exercício 01</h1>
-    <hr>
-    <p><i>Faça as chamadas e testes nesta página</i></p>
 
-    
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Exercício 02</title>
+</head>
+
+<body>
+	<?php
+	require_once "./src/Livro.php";
+
+	$livro = new Livro("Harry Potter e a Pedra Filosofal", "J.K. Rowling", 200);
+	?>
+
+	<h1>Exercício 02</h1>
+	<hr>
+
+	<h2>Descrição do livro 1</h2>
+	<ul>
+		<li><b>Titulo do livro: </b><?= $livro->getTitulo() ?></li>
+		<li><b>Autor: </b><?= $livro->getAutor() ?></li>
+		<li><b>Quantidade de páginas: </b><?= $livro->getPaginas() ?></li>
+	</ul>
+
 </body>
 </html>
--- src/Livro.php
+++ src/Livro.php
@@ -6,17 +6,41 @@ class Livro
   private string $autor;
   private int $paginas;
 
+  public function __construct(string $titulo, string $autor, int $paginas)
+  {
+    $this->setTitulo($titulo);
+    $this->setAutor($autor);
+    $this->setPaginas($paginas);
+  }
+
   private function setTitulo(string $titulo): void
   {
     $this->titulo = $titulo;
   }
 
-  private function setAutor(string $autor)
+  private function setAutor(string $autor): void
   {
     $this->autor = $autor;
   }
 
-  private function setPaginas(){
-    
+  private function setPaginas(int $paginas): void
+  {
+    $this->paginas = $paginas;
+  }
+
+
+  public function getTitulo()
+  {
+    return $this->titulo;
+  }
+
+  public function getAutor()
+  {
+    return $this->autor;
+  }
+
+  public function getPaginas()
+  {
+    return $this->paginas;
   }
 }
